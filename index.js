48c2417f4f2ea475005e4d1d4f7434c751abd003
aminmshahnani <mshahnani.amin@gmail.com>
2023-10-15 15:16:00 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const app =  require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const port = 300;
+const port = 3000;
 
-server.listen(port, () => {
-    console.log(`Server is running on port ${podrt}`);
-});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
 io.on('connection', (socket) => {
